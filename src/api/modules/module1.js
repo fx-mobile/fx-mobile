6eb2480da4532dd0d2538270927b6e0fd7bebb1d
jeffy <caixiaobing@live.cn>
2020-08-03 15:29:48 +0800

    Update module1.js
    mock server

--- src/api/modules/module1.js
+++ src/api/modules/module1.js
@@ -7,7 +7,7 @@ export default {
         cache: true
     }, 
     login:{uri:'/v1/user/login'},
-    getUserInfo: { uri: 'https://www.easy-mock.com/mock/58ff650c739ac1685205a1a7/foresee/v1/user/getUserInfo' },
+    getUserInfo: { uri: 'https://www.easy-mock.com/mock/58ff650c739ac1685205a1a7/*/v1/user/getUserInfo' },
     getNsrInfo: { uri : '/v1/user/getNsrInfo'},
-    getMyTodo: { uri: 'https://www.easy-mock.com/mock/58ff650c739ac1685205a1a7/foresee/v1/user/getMyTodo'}
+    getMyTodo: { uri: 'https://www.easy-mock.com/mock/58ff650c739ac1685205a1a7/*/v1/user/getMyTodo'}
 }
